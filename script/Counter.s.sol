a8a79154a02ad342da3a34252a15d2717144ec11
marcuswentz <markzcollege404@gmail.com>
2025-02-15 15:22:57 -0500

    remapping

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,37 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
 
-import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import { UniswapV3Factory } from "v3-core/contracts/UniswapV3Factory.sol";
+// import { NonfungiblePositionManager } from "v3-periphery/contracts/NonfungiblePositionManager.sol";
+// import { SwapRouter } from "v3-periphery/contracts/SwapRouter.sol";
+// import { TickLens } from "@uniswap/v3-periphery/contracts/lenses/TickLens.sol";
+// import { TickLens } from "@uniswap/v3-periphery/contracts/lenses/TickLens.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract DeployUniswap {
 
-    function setUp() public {}
+    UniswapV3Factory public factory;
+    // NonfungiblePositionManager public positionManager;
+    // SwapRouter public swapRouter;
+    
+    // TickLens public tickLens;
 
-    function run() public {
-        vm.startBroadcast();
+    address public owner;
 
-        counter = new Counter();
+    constructor() {
+        owner = msg.sender;
+    }
 
-        vm.stopBroadcast();
+    function deploy() public {
+        require(msg.sender == owner, "Only owner can deploy");
+
+        // Deploy Uniswap V3 core contracts
+
+        factory = new UniswapV3Factory();
+        // positionManager = new NonfungiblePositionManager(address(factory));
+        // swapRouter = new SwapRouter(address(factory));
+        // tickLens = new TickLens(address(factory));
+
+
+        // Now you can initialize the contracts or deploy other dependencies
     }
 }
