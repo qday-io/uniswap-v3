e9376836565cd80cb7ebff776b287bcfccc9827a
marcuswentz <markzcollege404@gmail.com>
2025-02-15 18:49:06 -0500

    NonfungibleTokenPositionDescriptor test setup import

--- script/step1DeployFactorySwap.s.sol
+++ script/step1DeployFactorySwap.s.sol
@@ -9,15 +9,20 @@ import { NFTDescriptor } from "@uniswap/v3-periphery/contracts/libraries/NFTDesc
 // https://github.com/Uniswap/v3-periphery/issues/272
 // https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/NFTDescriptor.sol
 
+// import { NonfungibleTokenPositionDescriptor } from "@uniswap/v3-periphery/contracts/NonfungibleTokenPositionDescriptor.sol";
+
 import { NonfungiblePositionManager } from "@uniswap/v3-periphery/contracts/NonfungiblePositionManager.sol";
 
 contract DeployUniswap {
 
     UniswapV3Factory public factory;
     SwapRouter public swapRouter;
-    // NFTDescriptor modified from library to contract to deploy.
+    // // NFTDescriptor modified from library to contract to deploy.
     NFTDescriptor public nftDescriptor;
-    NonfungiblePositionManager public positionManager;
+
+    // NonfungibleTokenPositionDescriptor public nftPositionDescriptor;
+    
+    NonfungiblePositionManager public nftPositionManager;
 
     function deploy() public {
 
@@ -30,14 +35,20 @@ contract DeployUniswap {
             weth9Address
         );
 
-        // NFTDescriptor modified from library to contract to deploy.
+        // // NFTDescriptor modified from library to contract to deploy.
         nftDescriptor = new NFTDescriptor();
+
+
+
+        // nftPositionDescriptor = new NonfungibleTokenPositionDescriptor();
+
+
 
         // address weth9Address = address(0);
         // address factoryAddress = address(0);
-        address tokenDescriptorAddress = address(0);
+        address tokenDescriptorAddress = address(0); // address(nftDescriptor)
 
-        positionManager = new NonfungiblePositionManager(
+        nftPositionManager = new NonfungiblePositionManager(
             address(factory), // factoryAddress,
             weth9Address,
             tokenDescriptorAddress
