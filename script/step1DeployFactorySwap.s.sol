d20d86ec662fc6d92694d88f11c26c0f5ed160c9
marcuswentz <markzcollege404@gmail.com>
2025-02-15 18:34:42 -0500

    NFTDescriptor modified from library to contract to deploy

--- script/step1DeployFactorySwap.s.sol
+++ script/step1DeployFactorySwap.s.sol
@@ -4,14 +4,17 @@ pragma solidity 0.7.6;
 import { UniswapV3Factory } from "@uniswap/v3-core/contracts/UniswapV3Factory.sol";
 import { SwapRouter } from "@uniswap/v3-periphery/contracts/SwapRouter.sol";
 
-// https://github.com/Uniswap/v3-periphery/issues/272
+// NFTDescriptor modified from library to contract to deploy.
 import { NFTDescriptor } from "@uniswap/v3-periphery/contracts/libraries/NFTDescriptor.sol";
+// https://github.com/Uniswap/v3-periphery/issues/272
 // https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/NFTDescriptor.sol
 
 contract DeployUniswap {
 
     UniswapV3Factory public factory;
     SwapRouter public swapRouter;
+    // NFTDescriptor modified from library to contract to deploy.
+    NFTDescriptor public nftDescriptor;
 
     function deploy() public {
 
@@ -23,6 +26,9 @@ contract DeployUniswap {
             address(factory),
             weth9Address
         );
+
+        // NFTDescriptor modified from library to contract to deploy.
+        nftDescriptor = new NFTDescriptor();
 
     }
 }
